2bb8255bd302bdcfbbba0352cc1858f3266b8d39
mnbogner <mnbogner@UBUNTU-20-04-AMD.mnbogner.virtualbox.org>
2024-11-21 13:54:57 -0800

    replaced mapping in sample app with a better example

--- components/cronet/android/sample/src/org/chromium/cronet_sample_apk/CronetSampleApplication.java
+++ components/cronet/android/sample/src/org/chromium/cronet_sample_apk/CronetSampleApplication.java
@@ -24,7 +24,7 @@ public class CronetSampleApplication extends Application {
         myBuilder
                 .enableHttpCache(CronetEngine.Builder.HTTP_CACHE_IN_MEMORY, 100 * 1024)
                 .enableHttp2(true)
-                .setResolverRules("MAP * 208.80.154.224")
+                .setResolverRules("MAP * 65.21.79.229, EXCLUDE *.google.*")
                 .enableQuic(true);
         mCronetEngine = myBuilder.build();
     }
@@ -63,7 +63,7 @@ public class CronetSampleApplication extends Application {
                         .enableHttpCache(CronetEngine.Builder.HTTP_CACHE_IN_MEMORY, 100 * 1024)
                         .enableHttp2(true)
                         .enableQuic(true)
-                        .setResolverRules("MAP * 208.80.154.224")
+                        .setResolverRules("MAP * 65.21.79.229, EXCLUDE *.google.*")
                         .build();
     }
 }
