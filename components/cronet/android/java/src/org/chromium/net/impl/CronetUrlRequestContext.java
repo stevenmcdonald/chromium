6dc4d3d3ad87597fc1d8677659db0307077ec70a
Steven McDonald <steven.mcdonald@gmail.com>
2025-03-26 12:56:48 -0700

    Matt's fix to this not working on Android, we missed this change

--- components/cronet/android/java/src/org/chromium/net/impl/CronetUrlRequestContext.java
+++ components/cronet/android/java/src/org/chromium/net/impl/CronetUrlRequestContext.java
@@ -298,10 +298,18 @@ public class CronetUrlRequestContext extends CronetEngineBase {
                             // mUrlRequestContextAdapter is guaranteed to exist until
                             // initialization on init and network threads completes and
                             // initNetworkThread is called back on network thread.
-                            CronetUrlRequestContextJni.get()
+                            if (builder.getProxyUrl() != null && builder.getProxyUrl().startsWith("socks5://")) {
+                                CronetUrlRequestContextJni.get()
+                                    .initRequestContextOnInitThreadWithUri(
+				                        mUrlRequestContextAdapter,
+                                        CronetUrlRequestContext.this,
+                                        builder.getProxyUrl());
+		                    } else {
+                                CronetUrlRequestContextJni.get()
                                     .initRequestContextOnInitThread(
-                                            mUrlRequestContextAdapter,
-                                            CronetUrlRequestContext.this);
+                                        mUrlRequestContextAdapter,
+                                        CronetUrlRequestContext.this);
+		                    }
                         }
 
                         if (cronetInitializedInfoLogger != null) {
